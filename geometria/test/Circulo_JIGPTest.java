4d13aa066b71991bb5a24a0497dfd1f47e0da316
José Ignacio García Prieto <nachogarciaprieto@gmail.com>
2019-02-09 14:45:04 +0100

    Uff ahora sí.

---
+++ geometria/test/Circulo_JIGPTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Circulo_JIGPTest {
+
+	@Test
+	void testArea() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	void testPerimetro() {
+		fail("No implementado aun");
+	}
+
+}
